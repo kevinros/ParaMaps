8b8d01714b0d01ab8b6a073a19c223d1ca3918da
nkuang123 <nkuang@vassar.edu>
2017-11-30 21:33:34 -0500

    Added SentenceTokenizer application to main

--- main/java/main.java
+++ main/java/main.java
@@ -3,6 +3,8 @@ package main.java;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+
 import General_Tools.*;
 
 
@@ -10,18 +12,32 @@ public class main {
 
     public static void main(String[] args) throws Exception {
 
-        WhiteSpaceTokenizer tokenizer = new WhiteSpaceTokenizer();
+        SentenceTokenizer sTokenizer = new SentenceTokenizer();
+        WhiteSpaceTokenizer wsTokenizer = new WhiteSpaceTokenizer();
         WordFrequency wordFrequency = new WordFrequency();
         String filePath = "src/StringInput.txt";
         String newSentence = new String(Files.readAllBytes(Paths.get(filePath)));
-        ArrayList<String> tokens;
+        List sentences;
+        List tokens = new ArrayList<>();
 
-        tokenizer.tokenize(newSentence);
-        tokenizer.removePunctuations();
-        tokens = tokenizer.getTokens();
+        sTokenizer.tokenize(newSentence);
+        sentences = sTokenizer.getTokens();
+
+        for (Object s : sentences) {
+            wsTokenizer.tokenize((String)s);
+            wsTokenizer.removePunctuations();
+            tokens.addAll(wsTokenizer.getTokens());
+        }
+
+
 
         wordFrequency.findFrequency(tokens);
         wordFrequency.printFrequencyMap();
+        System.out.println("----------------------");
+
+        for (Object s : sentences) {
+            System.out.println(s);
+        }
 
         //System.out.println(System.getProperty("user.dir"));
 
