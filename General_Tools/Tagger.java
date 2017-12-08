baed3c38588d2f335e53f426b7e7f63670f3a0ce
Kevin Ros <kevinros1190@gmail.com>
2017-12-08 00:09:13 -0500

    Implemented Tagger class, STILL NEEDS TO BE TESTED

--- General_Tools/Tagger.java
+++ General_Tools/Tagger.java
@@ -1,5 +1,34 @@
 package General_Tools;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+//The following is based on OpenNLP online tutorial
+// for tagging parts of speech
 public class Tagger {
 
-}
+    InputStream inputStream = new
+            FileInputStream("C:/OpenNLP_models/en-pos-maxent.bin");
+    POSModel model = new POSModel(inputStream);
+    POSTaggerME tag = new POSTaggerME(model);
+
+    //Returned a tagged array from a given sentence
+    public String[] tag (String sentence) {
+        //Tokenizing the sentence using WhitespaceTokenizer class
+        WhitespaceTokenizer whitespaceTokenizer = WhitespaceTokenizer.INSTANCE;
+        String[] tokens = whitespaceTokenizer.tokenize(sentence);
+
+        //Generating tags
+        String[] tags = tagger.tag(tokens);
+        return tags;
+    }
+    
+
+
+
+
+
+    }
